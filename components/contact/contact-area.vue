a531d666f85587338226b9658a1a5dca965ad042
J0hun1 <144909866+J0hun1@users.noreply.github.com>
2025-04-21 12:32:38 +0800

    updated contact forms

--- components/contact/contact-area.vue
+++ components/contact/contact-area.vue
@@ -112,7 +112,7 @@
                 <svg
                   width="12"
                   height="12"
-                  viewBox="0 0 12 12"
+                  viewBox="0 0 14 14"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
                 >
--- components/forms/contact-form.vue
+++ components/forms/contact-form.vue
@@ -1,60 +1,86 @@
 <template>
   <form id="contact-form" @submit="onSubmit">
-      <div class="tp-contact-input-wrapper">
-        <div class="tp-contact-input-box">
-            <div class="tp-contact-input">
-              <input name="name" id="name" type="text" placeholder="Shahnewaz Sakil" v-bind="name">
-            </div>
-            <div class="tp-contact-input-title">
-              <label for="name">Your Name</label>
-            </div>
-            <err-message :msg="errors.name" />
+    <div class="tp-contact-input-wrapper">
+      <div class="tp-contact-input-box">
+        <div class="tp-contact-input">
+          <input
+            name="name"
+            id="name"
+            type="text"
+            placeholder="Jose Rizal"
+            v-bind="name"
+          />
         </div>
-        <div class="tp-contact-input-box">
-            <div class="tp-contact-input">
-              <input name="email" id="email" type="email" placeholder="shofi@mail.com" v-bind="email">
-            </div>
-            <div class="tp-contact-input-title">
-              <label for="email">Your Email</label>
-            </div>
-            <err-message :msg="errors.email" />
+        <div class="tp-contact-input-title">
+          <label for="name">Your Name</label>
         </div>
-        <div class="tp-contact-input-box">
-            <div class="tp-contact-input">
-              <input name="subject" id="subject" type="text" placeholder="Write your subject" v-bind="subject">
-            </div>
-            <div class="tp-contact-input-title">
-              <label for="subject">Subject</label>
-            </div>
-            <err-message :msg="errors.subject" />
-        </div>
-        <div class="tp-contact-input-box">
-            <div class="tp-contact-input">
-              <Field name="message" v-slot="{ field }">
-                <textarea v-bind="field" id="message" name="message" placeholder="Write your message here..."></textarea>
-              </Field>
-            </div>
-            <div class="tp-contact-input-title">
-              <label for="message">Your Message</label>
-            </div>
-            <err-message :msg="errors.message" />
-        </div>
+        <err-message :msg="errors.name" />
       </div>
-      <div class="tp-contact-suggetions mb-20">
-        <div class="tp-contact-remeber">
-            <input id="remeber" type="checkbox">
-            <label for="remeber">Save my name, email, and website in this browser for the next time I comment.</label>
+      <div class="tp-contact-input-box">
+        <div class="tp-contact-input">
+          <input
+            name="email"
+            id="email"
+            type="email"
+            placeholder="freshbytes@gmail.com"
+            v-bind="email"
+          />
         </div>
+        <div class="tp-contact-input-title">
+          <label for="email">Your Email</label>
+        </div>
+        <err-message :msg="errors.email" />
       </div>
-      <div class="tp-contact-btn">
-        <button type="submit">Send Message</button>
+      <div class="tp-contact-input-box">
+        <div class="tp-contact-input">
+          <input
+            name="subject"
+            id="subject"
+            type="text"
+            placeholder="Write your subject"
+            v-bind="subject"
+          />
+        </div>
+        <div class="tp-contact-input-title">
+          <label for="subject">Subject</label>
+        </div>
+        <err-message :msg="errors.subject" />
       </div>
+      <div class="tp-contact-input-box">
+        <div class="tp-contact-input">
+          <Field name="message" v-slot="{ field }">
+            <textarea
+              v-bind="field"
+              id="message"
+              name="message"
+              placeholder="Write your message here..."
+            ></textarea>
+          </Field>
+        </div>
+        <div class="tp-contact-input-title">
+          <label for="message">Your Message</label>
+        </div>
+        <err-message :msg="errors.message" />
+      </div>
+    </div>
+    <div class="tp-contact-suggetions mb-20">
+      <div class="tp-contact-remeber">
+        <input id="remeber" type="checkbox" />
+        <label for="remeber"
+          >Save my name, email, and website in this browser for the next time I
+          comment.</label
+        >
+      </div>
+    </div>
+    <div class="tp-contact-btn">
+      <button type="submit">Send Message</button>
+    </div>
   </form>
 </template>
 
-<script setup lang="ts"> 
-import { useForm,Field } from 'vee-validate';
-import * as yup from 'yup';
+<script setup lang="ts">
+import { useForm, Field } from "vee-validate";
+import * as yup from "yup";
 
 interface IFormValues {
   name?: string | null;
@@ -62,21 +88,22 @@ interface IFormValues {
   subject?: string | null;
   message?: string | null;
 }
-const { errors, handleSubmit, defineInputBinds,resetForm } = useForm<IFormValues>({
-  validationSchema: yup.object({
-    name: yup.string().required().label("Name"),
-    email: yup.string().required().email().label("Email"),
-    subject: yup.string().required().label("Subject"),
-    message: yup.string().required().label("Message")
-  }),
-});
+const { errors, handleSubmit, defineInputBinds, resetForm } =
+  useForm<IFormValues>({
+    validationSchema: yup.object({
+      name: yup.string().required().label("Name"),
+      email: yup.string().required().email().label("Email"),
+      subject: yup.string().required().label("Subject"),
+      message: yup.string().required().label("Message"),
+    }),
+  });
 
-const onSubmit = handleSubmit(values => {
+const onSubmit = handleSubmit((values) => {
   alert(JSON.stringify(values, null, 2));
-  resetForm()
+  resetForm();
 });
 
-const name = defineInputBinds('name');
-const email = defineInputBinds('email');
-const subject = defineInputBinds('subject');
+const name = defineInputBinds("name");
+const email = defineInputBinds("email");
+const subject = defineInputBinds("subject");
 </script>
